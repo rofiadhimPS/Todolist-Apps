d2ac0a39981bbc63284348d6886fe77a8b8a4a03
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-25 18:15:10 +0700

    test show todolist

---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+
+$todoList[1] = "Study PHP Basic";
+$todoList[2] = "Study PHP OOP";
+$todoList[3] = "Study PHP Web";
+
+showTodoList();
