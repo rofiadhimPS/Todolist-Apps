35d3e4d32882ba10129e4bf301fcc71616112802
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-25 18:00:29 +0700

    create view

--- App.php
+++ App.php
@@ -4,6 +4,9 @@ require_once "Model/TodoList.php";
 require_once "BusinessLogic/ShowTodoList.php";
 require_once "BusinessLogic/AddTodoList.php";
 require_once "BusinessLogic/RemoveTodoList.php";
+require_once "View/ViewShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
 
 echo "Todo List Application" . PHP_EOL;
 
