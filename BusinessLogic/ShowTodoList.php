82fbea401c2c0e55b98f085c339ab64ac4ebbde4
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-25 18:04:54 +0700

    logic show todolist

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -3,5 +3,11 @@
 // Show todo in list
 function showTodoList()
 {
+    global $todoList;
 
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $value){
+        echo "$number. $value" . PHP_EOL;
+    };
 }
--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -2,5 +2,11 @@
 
 function viewShowTodoList()
 {
-    
+    global $todoList;
+
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach ($todoList as $number => $value){
+        echo "$number. $value" . PHP_EOL;
+    };
 }
