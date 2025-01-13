15a9f08cf47ad9a9955ab9a471acac6889e46986
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 07:53:47 +0700

    fix logic remove todolist

--- BusinessLogic/RemoveTodoList.php
+++ BusinessLogic/RemoveTodoList.php
@@ -9,8 +9,14 @@ function removeTodoList(int $number)
     }
 
     for ($i = $number; $i < sizeof($todoList); $i++){
-        $todoList[$i] = $todoList[$i] + 1;
+        $todoList[$i] = $todoList[$i+1];
     }
+
+    // 1. Bob
+    // 2. Roney
+    // 3. Rio
+    // 4. Andi
+    // 5. Max 
 
     unset($todoList[sizeof($todoList)]);
 
