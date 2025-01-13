5511c0aa700314954dcd338136e94b6b2746ebf9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 08:38:19 +0700

    view remove todolist

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,6 +1,24 @@
 <?php
 
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
 function viewRemoveTodoList()
 {
-    
+    echo "MENGHAPUS TODO" . PHP_EOL;
+
+    $pilihan = input("Nomor (x untuk batal)");
+
+    if($pilihan == "x"){
+        echo "Batal menghapus todo";
+    } else {
+        $success = removeTodoList($pilihan);
+
+        if ($success) {
+            echo "Sukses menghapus todo nomor $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus todo nomor $pilihan" . PHP_EOL;
+        }
+    }
+
 }
