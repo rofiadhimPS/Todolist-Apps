1b1059be0f74aadd3695cf23e0042755274b4f01
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 08:30:32 +0700

    test view addtodolist

---
+++ Test/TestViewAddTodoList.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "../View/ViewAddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Muh");
+addTodoList("Rofiadhim");
+addTodoList("Rajab");
+
+viewAddTodoList();
+
+showTodoList();
+
+viewAddTodoList();
+
+showTodoList();
--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -8,10 +8,10 @@ function viewAddTodoList()
 {
     echo "MENAMBAH TODO" . PHP_EOL;
 
-    $todo = input("Todo (x untuk batal) : ");
+    $todo = input("Todo (x untuk batal)");
 
     if ($todo == "x") {
-        // batal
+        echo "Batal menambah todo";
     } else {
         addTodoList($todo);
     }
