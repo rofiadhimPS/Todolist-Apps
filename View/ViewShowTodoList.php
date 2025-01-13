2a4c94e979d8746e7b59016fc7b116980737f4a1
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 08:10:38 +0700

    view showtodolist

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,12 +1,33 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 function viewShowTodoList()
 {
-    global $todoList;
+    showTodoList();
 
-    echo "TODOLIST" . PHP_EOL;
+    echo "MENU" . PHP_EOL;
+    echo "1. Tambah Todo" . PHP_EOL;
+    echo "2. Hapus Todo" . PHP_EOL;
+    echo "3. Keluar" . PHP_EOL;
 
-    foreach ($todoList as $number => $value){
-        echo "$number. $value" . PHP_EOL;
-    };
+    while (true) {
+        
+        $pilihan = input("Pilih");
+
+        if ($pilihan == "1"){
+            viewAddTodoList();
+        } else if ($pilihan == "2"){
+            viewRemoveTodoList();
+        } else if ($pilihan == "3"){
+            break;
+        } else {
+            "Pilihan tidak dimengerti";
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
