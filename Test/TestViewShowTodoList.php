a187ab43043e2fdc516645c5d6a3f0626aaaccaa
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 08:22:59 +0700

    test view showtodolist

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../View/ViewShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Muh");
+addTodoList("Rofiadhim");
+addTodoList("Rajab");
+
+viewShowTodoList();
--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "../Model/TodoList.php";
 require_once "../BusinessLogic/ShowTodoList.php";
 require_once "../View/ViewAddTodoList.php";
 require_once "../View/ViewRemoveTodoList.php";
@@ -7,25 +8,24 @@ require_once "../Helper/Input.php";
 
 function viewShowTodoList()
 {
-    showTodoList();
-
-    echo "MENU" . PHP_EOL;
-    echo "1. Tambah Todo" . PHP_EOL;
-    echo "2. Hapus Todo" . PHP_EOL;
-    echo "3. Keluar" . PHP_EOL;
-
     while (true) {
-        
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
         $pilihan = input("Pilih");
 
         if ($pilihan == "1"){
             viewAddTodoList();
         } else if ($pilihan == "2"){
             viewRemoveTodoList();
-        } else if ($pilihan == "3"){
+        } else if ($pilihan == "x"){
             break;
         } else {
-            "Pilihan tidak dimengerti";
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
         }
     }
 
